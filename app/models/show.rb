c859667453ae873ffa8f91175c873de1e165a5ec
Andrew Stahl <andrewstahl96@gmail.com>
2022-09-21 21:37:51 -0400

    Migrations created, methods established, all tests passed

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    list_of_actors = []
+    self.actors.each do |actor|
+      list_of_actors << actor.full_name
+    end
+    list_of_actors
+  end
 end
